c78d63f7f8504ad84a57a25fc45ec8f8ea7c9807
MICastilloHurtado <122422130+MICastilloHurtado@users.noreply.github.com>
2023-02-25 22:42:09 -0500

    Login 1

---
+++ main.js
@@ -0,0 +1,13 @@
+function login(){
+    var user, password
+
+    user = document.getElementById("usuario").value;
+    password = document.getElementById("contraseña").value;
+    
+    if(user == "prueba" && password == "1234"){
+        alert("Iniciaste Sesion!")
+    }else{
+        alert("Datos Incorrectos")
+    }
+
+} 
